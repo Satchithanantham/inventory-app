4bc3d950ba777901761429cceda607ce928ce44a
Balamurugan <satchithanantham.balamurugan@hidglobal.com>
2025-12-19 02:52:22 +0530

    Updated

--- Terraform/modules/s3_logs/main.tf
+++ Terraform/modules/s3_logs/main.tf
@@ -1,24 +1,12 @@
 data "aws_caller_identity" "current" {}
 
 # S3 BUCKET FOR ALB ACCESS LOGS
-
 resource "aws_s3_bucket" "alb_logs" {
   bucket = "inventory-alb-access-logs-${var.env}-${random_id.suffix.hex}"
 }
 
 resource "random_id" "suffix" {
   byte_length = 4
-}
-
-
-# BLOCK PUBLIC ACCESS (MANDATORY)
-
-resource "aws_s3_bucket_public_access_block" "alb_logs" {
-  bucket                  = aws_s3_bucket.alb_logs.id
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
 }
 
 ################################
@@ -54,7 +42,6 @@ resource "aws_s3_bucket_policy" "alb_logs" {
             "s3:x-amz-acl" = "bucket-owner-full-control"
           }
         }
-
       }
     ]
   })
